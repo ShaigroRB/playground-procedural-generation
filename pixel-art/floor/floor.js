321ec88cdf883db6b5733cea8a9b1c9861eaa34c
ShaigroRB <robin.boucher@epita.fr>
2022-01-17 22:55:50 +0100

    refactor(floor): refactoring of the code due to blog post

--- pixel-art/floor/floor.js
+++ pixel-art/floor/floor.js
@@ -1,12 +1,12 @@
 // ------- Define stuff
 
-// Img sizes
+// Tile sizes
 let width = 32;
 let height = 32;
 
 // plank
 let nbPlanks = 8;
-let spaceBetweenVertDelim = Math.floor(width / 6);
+let spaceBetweenVertOutlines = Math.floor(width / 6);
 
 let shadingPercentage = -2;
 let areColorsGenerated = true;
@@ -19,7 +19,7 @@ let distanceBetweenPlanks = height / nbPlanks;
 // colors
 let defaultPlankColor = "#96704A";
 let darkerPlankColor = "#916B44";
-let delimPlankColor = "#815D34";
+let outlinesPlankColor = "#815D34";
 let intersectionColor = "#73532E";
 let defaultBaseSchemeColor = "#96704A";
 
@@ -30,6 +30,9 @@ let nbVariations = 1;
 let variationsStages = [];
 
 //#region Utility functions
+// Get seeded random number between min & max.
+const GetNextNumber = (nextNumber, min, max) =>
+    Math.floor(nextNumber() * (max - min) + min);
 
 /**
  * Make a random id
@@ -90,12 +93,15 @@ function shadeColor(color, percent) {
     G = (G < 255) ? G : 255;
     B = (B < 255) ? B : 255;
 
-    var RR = ((R.toString(16).length == 1) ? "0" + R.toString(16) : R.toString(16));
-    var GG = ((G.toString(16).length == 1) ? "0" + G.toString(16) : G.toString(16));
-    var BB = ((B.toString(16).length == 1) ? "0" + B.toString(16) : B.toString(16));
+    var RR = ((R.toString(16).length === 1) ? "0" + R.toString(16) : R.toString(16));
+    var GG = ((G.toString(16).length === 1) ? "0" + G.toString(16) : G.toString(16));
+    var BB = ((B.toString(16).length === 1) ? "0" + B.toString(16) : B.toString(16));
 
     return "#" + RR + GG + BB;
 }
+
+// Create a point
+const CreatePoint = (x, y) => ({ x, y });
 //#endregion
 
 //#region Draw figures on canvas
@@ -134,87 +140,94 @@ function DrawKonvaLetterT(group, color) {
 //#endregion
 
 //#region Draw elements of floor texture (planks, delimitations, intersections)
-/**
- * Draw horizontal delimitations for planks
- * @param {HTMLCanvasElement} canvas 
- * @param {number} distance 
- * @param {(start: {x: number, y: number}, end: {x: number, y: number}) => void} drawLine 
- */
-function DrawHorizDelim(height, width, distance, drawLine) {
+function DrawHorizOutlines(
+    // height & width of the tile
+    height, width,
+    distanceBetweenPlanks,
+    // takes start & end points to draw a line (from DrawKonvaLine)
+    drawLine) {
     let indexHeight = 0;
 
     while (indexHeight < height) {
-        indexHeight += (distance - 1);
+        indexHeight += (distanceBetweenPlanks - 1);
 
         // 0.5 are for anti-aliasing, cf MDN: 
         // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Applying_styles_and_colors#a_linewidth_example
-        const start = {
-            x: 0,
-            y: indexHeight + 0.5
-        };
-        const end = {
-            x: width,
-            y: indexHeight + 0.5
-        };
+        const start = CreatePoint(0, indexHeight + 0.5)
+        const end = CreatePoint(width, indexHeight + 0.5)
 
         drawLine(start, end);
         indexHeight++;
     }
 }
 
-function DrawVertDelim(height, width, distance, nextNumber, drawLine) {
-    let indexHeight = 0;
-    let intersections = [];
-    let previousX = -50;
+// Getting two outlines that are really close to each other is ugly.
+// So, an adjustment is needed.
+const adjustX = (
+    x, previousX,
+    distanceBetweenOutlines,
+    nextNumber) => {
+    const diff = Math.abs(previousX - x);
+    let adjustedX = x;
 
-    // Getting two delimitations that are really close to each other is ugly
-    // So, an adjustment is needed.
-    const adjustX = (x) => {
-        const diff = Math.abs(previousX - x);
-        let correctedX = x;
-
-        // Adjust the space with the previous delimitation
-        if (diff <= spaceBetweenVertDelim) {
-            correctedX = x < previousX ? previousX - spaceBetweenVertDelim : previousX + spaceBetweenVertDelim;
-        }
-        if (diff == 0) {
-            const isLeft = Math.floor(2 * nextNumber()) === 1;
-            correctedX = isLeft ? previousX - spaceBetweenVertDelim : previousX + spaceBetweenVertDelim;
-        }
-
-        return correctedX;
+    // Adjust the distance with previous outline.
+    if (diff <= distanceBetweenOutlines) {
+        // If outline is on the left of previous one.
+        adjustedX = x < previousX
+            ? previousX - distanceBetweenOutlines
+            : previousX + distanceBetweenOutlines;
+    }
+    // If outline is at the same location than the previous one.
+    if (diff === 0) {
+        // Randomly decides its location.
+        const isOutlineOnLeft = Math.floor(2 * nextNumber()) === 1;
+        adjustedX = isOutlineOnLeft
+            ? previousX - distanceBetweenOutlines
+            : previousX + distanceBetweenOutlines;
     }
 
+    return adjustedX;
+}
+
+function DrawVertOutlines(
+    height, width,
+    distanceBetweenPlanks, distanceBetweenOutlines,
+    nextNumber, drawLine) {
+    let indexHeight = 0;
+    let previousX = -50;
+    let intersections = [];
+
     const getRandomX = () => {
-        const max = width - 1 - spaceBetweenVertDelim;
-        const min = spaceBetweenVertDelim;
+        // Default max & min using distance between outlines
+        // to avoid adjusting an outline outside of the tile.
+        const max = width - 1 - distanceBetweenOutlines;
+        const min = distanceBetweenOutlines;
+
+        // Random seeded number returned by nextNumber
         return Math.floor(nextNumber() * (max - min + 1) + min);
     }
 
     while (indexHeight < height) {
+        // random coordinate for the outline
         let x = getRandomX();
 
         // Adjust the X if needed
-        x = adjustX(x);
+        x = adjustX(x, previousX, distanceBetweenOutlines, nextNumber);
         previousX = x;
 
         // anti-aliasing fix
         x += 0.5;
 
-        // from top to bottom
-        const start = {
-            x: x,
-            y: indexHeight - 1
-        };
-        const end = {
-            x: x,
-            y: indexHeight + (distance - 1)
-        };
+        // top of outline
+        const start = CreatePoint(x, indexHeight - 1);
+        // bottom of outline
+        const end = CreatePoint(x, indexHeight + (distanceBetweenPlanks - 1));
 
         drawLine(start, end);
-        indexHeight += distance;
 
-        // add new intersections
+        indexHeight += distanceBetweenPlanks;
+
+        // Add new intersections.
         intersections = [
             ...intersections,
             [start, end]
@@ -224,56 +237,126 @@ function DrawVertDelim(height, width, distance, nextNumber, drawLine) {
     return intersections;
 }
 
-function CreatePoint(x, y) {
+// Get points from intersection.
+const getTopPoint = (intersection) => intersection[0];
+const getBottomPoint = (intersection) => intersection[1];
+// Vertical spreading based on a point.
+const verticalSpreadingFromPoint = (point, spreading) => ({
+    center: CreatePoint(point.x, point.y),
+    bottom: CreatePoint(point.x, point.y + spreading)
+});
+
+function setVerticalSpreading(
+    intersection, nextNumber,
+    minVertSpread, maxVertSpread) {
+    // Should darken top intersection of outline?
+    const shouldDarkenTop = nextNumber() > 0.5;
+    const direction = (shouldDarkenTop ? 1 : -1);
+    const vertSpread =
+        GetNextNumber(nextNumber, minVertSpread, maxVertSpread) * direction;
+    const chosenPoint = shouldDarkenTop
+        ? getTopPoint(intersection)
+        : getBottomPoint(intersection);
+
+    return verticalSpreadingFromPoint(chosenPoint, vertSpread);
+}
+
+function shouldIntersectionsBeConnected(
+    point, previous,
+    distanceBetweenOutlines) {
+    let result = {
+        shouldBeConnected: false,
+        isLeftPoint: false
+    };
+
+    // If previous intersection is not on same abscissa as current one.
+    if (previous.y !== point.y) {
+        return result;
+    }
+
+    // If diff > 0, previous is on left.
+    // If diff < 0, previous is on right.
+    const diff = point.x - previous.x;
+    const shouldBeConnected = Math.abs(diff) <= distanceBetweenOutlines;
+
     return {
-        x: x,
-        y: y
+        shouldBeConnected,
+        isLeftPoint: diff > 0
+    }
+}
+
+function setHorizontalSpreading(
+    centerPoint, previous,
+    distanceBetweenOutlines, nextNumber,
+    minHorizSpread, maxHorizSpread) {
+    // Formula to determine how much left/right branches spread.
+    const computeBranchSpreading =
+        () => GetNextNumber(nextNumber, minHorizSpread, maxHorizSpread);
+    let leftSpreading = computeBranchSpreading();
+    let rightSpreading = computeBranchSpreading();
+
+    // If intersections should be connected.
+    const { shouldBeConnected, isLeftPoint } =
+        shouldIntersectionsBeConnected(centerPoint, previous, distanceBetweenOutlines);
+
+    // Set the left/right spreading.
+    if (shouldBeConnected) {
+        const diff = Math.abs(previous.x - centerPoint.x);
+        if (isLeftPoint) {
+            leftSpreading = diff;
+        } else {
+            rightSpreading = diff;
+        }
+    }
+
+    // Anti-aliasing fix.
+    const tmpHorizPoint = CreatePoint(centerPoint.x - 0.5, centerPoint.y + 0.5);
+
+    // Left/right spread of intersection for "T".
+    return {
+        left: CreatePoint(tmpHorizPoint.x - leftSpreading, tmpHorizPoint.y),
+        right: CreatePoint(tmpHorizPoint.x + rightSpreading, tmpHorizPoint.y)
     };
 }
 
-function UpdatePoint(point, x, y) {
-    point.x = x;
-    point.y = y;
-}
+function DrawIntersections(
+    distanceBetweenOutlines, distanceBetweenPlanks,
+    intersections,
+    nextNumber, drawLetterT) {
+    // Max spreads.
+    // Spread is how far intersection is darkened.
+    const maxVertSpread = Math.floor(distanceBetweenPlanks / 2);
+    const minVertSpread = Math.floor(maxVertSpread / 4);
+    const maxHorizSpread = distanceBetweenOutlines / 2;
+    const minHorizSpread = Math.floor(maxHorizSpread / 3);
 
-function DrawIntersections(intersections, nextNumber, drawLetterT) {
-    let leftPoint = CreatePoint(0, 0);
-    let rightPoint = CreatePoint(0, 0);
-    let centerPoint = CreatePoint(0, 0);
-    let bottomPoint = CreatePoint(0, 0);
-
-    const getTopPoint = (intersection) => intersection[0];
-    const getBottomPoint = (intersection) => intersection[1];
+    // Previous darkened intersection.
+    let previous = CreatePoint(-50, -50);
 
     intersections.forEach((intersection) => {
-        const isIntersection = nextNumber() < 0.5;
-
-        if (!isIntersection) {
+        // Should intersection be darkened?
+        const shouldBeDarkened = nextNumber() < 0.5;
+        if (!shouldBeDarkened) {
             return;
         }
 
-        const isIntersectionTop = nextNumber() > 0.5;
+        // Vertical spread
+        const { center, bottom } = setVerticalSpreading(
+            intersection, nextNumber,
+            minVertSpread, maxVertSpread);
 
-        if (isIntersectionTop) {
-            const tmpVertiPoint = getTopPoint(intersection);
-            UpdatePoint(centerPoint, tmpVertiPoint.x, tmpVertiPoint.y);
-            UpdatePoint(bottomPoint, tmpVertiPoint.x, tmpVertiPoint.y + 2);
-        } else {
-            const tmpVertiPoint = getBottomPoint(intersection);
-            UpdatePoint(centerPoint, tmpVertiPoint.x, tmpVertiPoint.y);
-            UpdatePoint(bottomPoint, tmpVertiPoint.x, tmpVertiPoint.y - 2);
-        }
+        // Horizontal spread
+        const { left, right } = setHorizontalSpreading(
+            center, previous,
+            distanceBetweenOutlines, nextNumber,
+            minHorizSpread, maxHorizSpread
+        );
 
-        const leftSpreading = Math.floor((nextNumber() / 0.30)) + 2;
-        const rightSpreading = Math.floor((nextNumber() / 0.30)) + 2;
+        // Draw the "T" that will "darken" the intersection.
+        drawLetterT(left, right, center, bottom);
 
-        //anti-aliasing fix
-        const tmpHorizPoint = CreatePoint(centerPoint.x - 0.5, centerPoint.y + 0.5);
-
-        UpdatePoint(leftPoint, tmpHorizPoint.x - leftSpreading, tmpHorizPoint.y);
-        UpdatePoint(rightPoint, tmpHorizPoint.x + rightSpreading, tmpHorizPoint.y);
-
-        drawLetterT(leftPoint, rightPoint, centerPoint, bottomPoint);
+        // Update previous intersection.
+        previous = center;
     });
 }
 
@@ -295,76 +378,92 @@ function DrawPlanks(height, width, distanceBetweenPlanks, nextNumber, drawPlank,
 //#endregion
 
 //#region Generation functions
-function GeneratePreview(stageWidth, stageHeight, groups, previewStage, scale) {
-    const getRandomGroup = () => groups[Math.floor(Math.random() * groups.length)];
+function GeneratePreview(width, height, tiles, previewStage, scale) {
+    const cloneTile = (tile, x, y) =>
+        (tile.clone({ x, y }));
+    const getRandomTile = () => tiles[Math.floor(Math.random() * tiles.length)];
 
+    // Used to copy/paste tile on layer.
     let layer = new Konva.Layer();
 
     // Join random groups together to create the preview
-    for (let x = 0; x < stageWidth * scale; x += stageWidth) {
-        for (let y = 0; y < stageHeight * scale; y += stageHeight) {
-            const group = getRandomGroup();
-            const newGroup = group.clone({
-                x: x,
-                y: y
-            });
-            layer.add(newGroup);
+    for (let x = 0; x < width * scale; x += width) {
+        for (let y = 0; y < height * scale; y += height) {
+            const tile = getRandomTile();
+            // Copy & Paste the tile.
+            const clone = cloneTile(tile, x, y);
+            layer.add(clone);
         }
     }
 
+    // Draw the preview.
     previewStage.add(layer);
 }
 
 function Generate(seed) {
+    // Seeded random number generator.
     const genRnd = new Math.seedrandom(seed);
 
-    let plankGroup = new Konva.Group();
-    let delimGroup = new Konva.Group();
-    let intersectGroup = new Konva.Group();
+    /// Konva groups
+    // Planks & horizontal outlines are reusable for variations.
+    let reusableGroup = new Konva.Group();
+    let othersGroup = new Konva.Group();
 
-    const drawKonvaRectLightPlankColor = DrawKonvaRect(plankGroup, defaultPlankColor);
-    const drawKonvaRectDarkPlankColor = DrawKonvaRect(plankGroup, darkerPlankColor);
-    const drawKonvaLine = DrawKonvaLine(delimGroup, delimPlankColor);
-    const drawKonvaLetterT = DrawKonvaLetterT(intersectGroup, intersectionColor);
+    /// Functions to draw shapes.
+    const drawKonvaRectLightPlankColor = DrawKonvaRect(reusableGroup, defaultPlankColor);
+    const drawKonvaRectDarkPlankColor = DrawKonvaRect(reusableGroup, darkerPlankColor);
+    const drawKonvaLine = (group) => DrawKonvaLine(group, outlinesPlankColor);
+    const drawKonvaLetterT = DrawKonvaLetterT(othersGroup, intersectionColor);
 
+    // Add planks to reusable group.
     DrawPlanks(
         height, width,
         distanceBetweenPlanks, genRnd,
         drawKonvaRectLightPlankColor, drawKonvaRectDarkPlankColor
     );
+    // Add horizontal outlines to reusable group.
+    DrawHorizOutlines(height, width, distanceBetweenPlanks, drawKonvaLine(reusableGroup));
 
-    const drawDelimAndIntersections = (stage) => {
-        // Create delimitations between planks
-        DrawHorizDelim(height, width, distanceBetweenPlanks, drawKonvaLine);
-        const intersections = DrawVertDelim(height, width, distanceBetweenPlanks, genRnd, drawKonvaLine);
+    const drawVertOutlinesAndIntersections = (stage) => {
+        // Add vertical outlines to non-reusables group.
+        // Remove first because no horizontal outline at the top.
+        const [, ...intersections] = DrawVertOutlines(
+            height, width,
+            distanceBetweenPlanks, spaceBetweenVertOutlines,
+            genRnd, drawKonvaLine(othersGroup)
+        );
 
-        // Create intersections
-        DrawIntersections(intersections, genRnd, drawKonvaLetterT);
+        // Add intersections to non-reusables group.
+        DrawIntersections(
+            spaceBetweenVertOutlines, distanceBetweenPlanks,
+            intersections,
+            genRnd, drawKonvaLetterT
+        );
 
-        // Draw all groups onto the stage
-        const group = StackGroups([plankGroup, delimGroup, intersectGroup]);
+        // Draw all groups onto the stage.
+        const group = StackGroups([reusableGroup, othersGroup]);
         DrawGroupOntoStage(group, stage);
 
-        // Clear delimitations & intersections groups for future use
-        delimGroup.destroyChildren();
-        intersectGroup.destroyChildren();
+        // Clear non-reusables group for future use.
+        othersGroup.destroyChildren();
 
         return group;
     }
 
+    DrawGroupOntoStage(reusableGroup, stage);
+
     let groups = [];
 
     // First result
-    let tmpGroup = drawDelimAndIntersections(stage);
+    let tmpGroup = drawVertOutlinesAndIntersections(stage);
     groups.push(tmpGroup);
 
     // Variations
     for (let variationStage of variationsStages) {
-        tmpGroup = drawDelimAndIntersections(variationStage);
+        tmpGroup = drawVertOutlinesAndIntersections(variationStage);
         groups.push(tmpGroup);
     }
 
-    Clear(previewStage);
     GeneratePreview(width, height, groups, previewStage, previewScale);
 }
 
@@ -391,7 +490,7 @@ function GetSeed() {
 function GetCanvasAttributes() {
     width = parseInt(document.getElementById("canvas-width").value);
     const divisor = parseInt(document.getElementById("space-between-delim").value);
-    spaceBetweenVertDelim = Math.floor(width / divisor);
+    spaceBetweenVertOutlines = Math.floor(width / divisor);
 
     height = parseInt(document.getElementById("canvas-height").value);
 }
@@ -413,8 +512,8 @@ function GetColors() {
 
         defaultPlankColor = defaultBaseSchemeColor;
         darkerPlankColor = shadeColor(defaultPlankColor, shadingPercentage);
-        delimPlankColor = shadeColor(darkerPlankColor, shadingPercentage - 5);
-        intersectionColor = shadeColor(delimPlankColor, shadingPercentage - 2);
+        outlinesPlankColor = shadeColor(darkerPlankColor, shadingPercentage - 5);
+        intersectionColor = shadeColor(outlinesPlankColor, shadingPercentage - 2);
 
         return;
     }
@@ -422,7 +521,7 @@ function GetColors() {
     // if colors are defined manually
     defaultPlankColor = document.getElementById("light-plank-color").value;
     darkerPlankColor = document.getElementById("dark-plank-color").value;
-    delimPlankColor = document.getElementById("delimitation-color").value;
+    outlinesPlankColor = document.getElementById("delimitation-color").value;
     intersectionColor = document.getElementById("intersection-color").value;
 }
 
@@ -432,7 +531,6 @@ function setVariationsStages() {
     // Clear the previous variations
     variationsStages = [];
     variationsDiv.textContent = '';
-
 
     for (let i = 0; i < nbVariations; i++) {
         // Create div for the current variation
